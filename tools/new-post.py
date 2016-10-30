d731a9e2d178e9ceffe5d3a05351c60de30e105e
Dimitri Merejkowsky <d.merej@gmail.com>
2016-10-30 17:06:42 +0100

    Add a tool to create new post
    Yeah I know `hugo new` exists, but:
    
    * this one creates a nice filename with a date, but a date-free slug
    * it uses `yaml` syntax for the header which is nicer for multi-line
      strings such as summary
    * it adds the `<!--more-->` tag I can never remember
    * it creates a commit for me

---
+++ tools/new-post.py
@@ -0,0 +1,48 @@
+import os
+import re
+import subprocess
+import sys
+
+import arrow
+
+TEMPLATE="""\
+---
+slug: {slug}
+date: {full_date}
+draft: false
+title: {title}
+summary: |
+
+---
+
+
+<!--more-->
+"""
+
+
+def slugify(text):
+    res = re.sub('[^\w\s-]', '', text).strip().lower()
+    return re.sub('[-\s]+', '-', res)
+
+
+def main():
+    title = sys.argv[1]
+    slug = slugify(title)
+    now = arrow.get()
+    file_name = now.strftime("%Y-%m-%d") + "-" + slug + ".md"
+    full_date = arrow.get().isoformat()
+    post_path = os.path.join("content/post", file_name)
+    to_write = TEMPLATE.format(**locals())
+    with open(post_path, "w") as fp:
+        fp.write(to_write)
+    to_run = [
+        ("git" , "add", post_path),
+        ("git" , "commit", "--message", "New post: %s" % title),
+        ("/usr/bin/nvim", post_path),
+    ]
+    for cmd in to_run:
+        subprocess.check_call(cmd)
+
+
+if __name__ == "__main__":
+    main()
