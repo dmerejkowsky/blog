89deae01f6a1a79a4f8682344745fce7777c3ee3
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2019-03-24 16:48:35 +0100

    kak ftw

--- tools/new-post.py
+++ tools/new-post.py
@@ -57,7 +57,7 @@ def main():
     with open(post_path, "w") as fp:
         fp.write(to_write)
     subprocess.call(["git", "add", post_path])
-    subprocess.call(["nvim", post_path])
+    subprocess.call(["kak", post_path])
 
 
 if __name__ == "__main__":
